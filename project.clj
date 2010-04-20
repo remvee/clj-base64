7ddd4a5c1853889da14f0c5523252f6a1ae1e0a5
Remco van 't Veer <rwvtveer@xs4all.nl>
2010-04-20 16:15:08 +0200

    added lein-clojars dev-dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Functions to encode and decode base64 strings."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]]
   :namespaces [remvee.base64])
