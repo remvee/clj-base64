9ebf1ff2f85c7aa3853c4b1858e95b58037c38aa
Remco van 't Veer <rwvtveer@xs4all.nl>
2010-08-20 16:37:34 +0200

    setup autodoc

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
 (defproject clj-base64 "0.0.0-SNAPSHOT"
   :description "Functions to encode and decode base64 strings."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [leiningen/lein-swank "1.1.0"]]
-  :namespaces [remvee.base64])
+
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [autodoc "0.7.1"]]
+  
+  :autodoc {:description "Functions to encode and decode base64 strings."
+            :copyright "Copyright (c) Remco van 't Veer."
+            :web-src-dir "http://github.com/remvee/clj-base64/blob/"})
