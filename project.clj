dfaf9e36b8174d0fe7468bc0b5c1d3cea6411216
Remco van 't Veer <rwvtveer@xs4all.nl>
2011-03-10 08:22:44 +0100

    release 0.0.1

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject clj-base64 "0.0.0-SNAPSHOT"
+(defproject clj-base64 "0.0.1"
   :description "Functions to encode and decode base64 strings."
 
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [autodoc "0.7.1"]]
+  :dev-dependencies [[autodoc "0.7.1"]]
   
   :autodoc {:description "Functions to encode and decode base64 strings."
             :copyright "Copyright (c) Remco van 't Veer."
